671cb223c5179eb3ffa93531d2347591b4bc7c05
maulanapranasutra <maulana_pranasutra_26rpl@student.smktelkom-mlg.sch.id>
2019-01-20 17:27:31 +0700

    selesai

---
+++ application/views/v_keluarga.php
@@ -0,0 +1,128 @@
+<html>
+<head>
+<title></title>
+<style>
+    h1 {
+        text-align: center;
+        font-size: 70px;
+        font-family: chiller;
+        padding:50px 0px 20px 0px;
+        text-shadow: 2px 2px 2px black;
+
+      }
+</style>
+<marquee behavior="alternate" direction="up" scrollamount="3" scrolldelay="65" height="80" style="text-align" filter:wave></div>
+    <h1>My Family</h1></marquee>
+<br>
+<img src="<?= base_url('assets/images/Fam.jpg')?>" style="width:100%">
+<br><br>
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2 style= "text-align:center; font-family: comic san ms; font-size: 25px; color: brown">Ayah</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/bapak.jpg')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>Nama</td> <td>: Putut Kandar Cahyono</td>
+                            </tr>
+                            <tr>
+                                <td>Pekerjaan</td> <td>: Wiraswasta</td>
+                            </tr>
+                            <tr>
+                                <td>Umur</td> <td>: 45 tahun</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td> <td>: Perum Omaview GL-35</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2 style= "text-align:center; font-family: comic san ms; font-size: 25px; color: brown">Mama</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/ibuk.jpg')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>Nama</td> <td>: Pungky Trias Paramita</td>
+                            </tr>
+                            <tr>
+                                <td>Pekerjaan</td> <td>: Ibu Rumah Tangga</td>
+                            </tr>
+                            <tr>
+                                <td>Umur</td> <td>: 38 tahun</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td> <td>: Malang</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2 style= "text-align:center; font-family: comic san ms; font-size: 25px; color: brown">Adik</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/adek.jpg')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>Nama</td> <td>: Raka Bagas Pranatama</td>
+                            </tr>
+                            <tr>
+                                <td>Pekerjaan</td> <td>: Pelajar</td>
+                            </tr>
+                            <tr>
+                                <td>Umur</td> <td>: 9 tahun</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td> <td>: Malang</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+</head>
+
+</html>
---
+++ application/views/v_profil.php
@@ -0,0 +1,39 @@
+<<div class="block-header">
+    <h1 style="text-align: center">Biodata ku</h1>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2>Jati Diri</h2>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                        <div class="body">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <img src="<?= base_url('assets/images/profil1.jpg')?>" style="width:100%">
+                            </div>
+                            <div class="col-md-7">
+                            <table class="table table-hover table-striped">
+                            <tr>
+                                <td>Nama</td> <td>: Maulana Pranasutra Cahyoaji</td>
+                            </tr>
+                            <tr>
+                                <td>Kelas</td> <td>: XI RPL 2</td>
+                            </tr>
+                            <tr>
+                                <td>Umur</td> <td>: 16 tahun</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td> <td>: Malang</td>
+                            </tr>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
